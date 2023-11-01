1483d62e38bfa643009f7840096bbd9f3f0b81a7
jonecars <carsonearljone@gmail.com>
2023-11-01 09:47:38 -0600

    InterfaceUpdate
    outline

---
+++ interfaceElement.cpp
@@ -0,0 +1,57 @@
+#include "interfaceElement.h"
+
+void Interface::input(UserInput input)
+{
+}
+
+void Interface::processing()
+{
+}
+
+void Interface::output()
+{
+}
+
+void InterfacePellet::draw()
+{
+}
+
+void InterfaceMissle::draw()
+{
+}
+
+void InterfaceBomb::draw()
+{
+}
+
+void InterfaceShrapnel::draw()
+{
+}
+
+void InterfaceFragment::draw()
+{
+}
+
+void InterfaceExhaust::draw()
+{
+}
+
+void InterfaceStreek::draw()
+{
+}
+
+void InterfaceStandard::draw()
+{
+}
+
+void InterfaceSinker::draw()
+{
+}
+
+void InterfaceFloater::draw()
+{
+}
+
+void InterfaceCrazy::draw()
+{
+}
--- interfaceElement.h
+++ interfaceElement.h
@@ -1,5 +1,79 @@
 #pragma once
+
+
+class UserInput;
+class LogicElement;
 class Interface {
+private:
+	LogicElement logic;
+public:
+	void interface();
+	void input(UserInput input);
+	void processing();
+	void output();
 
 
+};
+
+
+class InterfaceElement {
+public:
+	virtual void draw() = 0;
+};
+
+
+
+class InterfacePellet {
+public:
+	void draw();
+};
+
+class InterfaceMissle {
+public:
+	void draw();
+};
+
+class InterfaceBomb {
+public:
+	void draw();
+};
+
+class InterfaceShrapnel {
+public:
+	void draw();
+};
+
+class InterfaceFragment {
+public:
+	void draw();
+};
+
+class InterfaceExhaust {
+public:
+	void draw();
+};
+
+class InterfaceStreek {
+public:
+	void draw();
+};
+
+class InterfaceStandard {
+public:
+	void draw();
+};
+
+class InterfaceSinker {
+public:
+	void draw();
+};
+
+class InterfaceFloater {
+public:
+	void draw();
+};
+
+class InterfaceCrazy {
+public:
+	void draw();
 };
