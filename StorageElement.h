422644ee02648a3043366be6fcb1604bdedebff3
Jeremy Busch <64992102+JeremyCBusch@users.noreply.github.com>
2023-11-01 09:54:44 -0600

    storage stubs except for effects

--- StorageElement.h
+++ StorageElement.h
@@ -10,6 +10,7 @@ private:
 	Position* pt;
 	Velocity* v;
 	double radius;
+	double speed;
 	int points;
 	bool isDead;
 	LogicElement* pLogic;
@@ -31,7 +32,43 @@ public:
 	void setisDead(bool isDead) { this->isDead = isDead; }
 	void setLogicElement(LogicElement* pLogic) { this->pLogic = pLogic; }
 	void setInterfaceElement(InterfaceElement* pInterface) { this->pInterface = pInterface; }
-
-
 };
 
+class StorageBird : public StorageElement
+{
+private:
+	double speed;
+public:
+	
+	StorageBird(double radius = 25.0, double speed = 5.0, int points = 10)
+	{
+		this->radius = radius;
+		this->speed = speed;
+		this->points = points;
+	}
+
+	double getSpeed() { return speed; }
+	void setSpeed(double speed) { this->speed = speed; }
+};
+
+class StorageBullet : public StorageElement
+{
+private:
+	double angle;
+	double speed;
+	double radius;
+	int value;
+public:
+	StorageBullet(double angle = 0.0, double speed = 30.0, double radius = 5.0, int value = 1)
+	{
+		this->angle = angle;
+		this->speed = speed;
+		this->radius = radius;
+		this->value = value;
+	}
+};
+
+class StorageEffects : public StorageElement
+{
+
+};
